0322ec1df9e1cc8c924d652094c3f96b44dec0cc
Dave Nicholson <me@davenicholson.xyz>
2026-03-06 19:02:58 +0000

    kitty config

--- home.nix
+++ home.nix
@@ -18,6 +18,7 @@ in
 
   home.stateVersion = "25.11"; 
 
+  home.file.".config/kitty".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/config/kitty";
   home.file.".config/quickshell".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/config/quickshell";
 
   home.packages = with pkgs; [
