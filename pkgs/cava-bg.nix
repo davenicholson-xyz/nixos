3df9263cb003ee4ccceabcbcd985ab05bc488fd8
Dave Nicholson <me@davenicholson.xyz>
2026-04-14 19:51:52 +0100

    cava-fix

--- pkgs/cava-bg.nix
+++ pkgs/cava-bg.nix
@@ -11,8 +11,13 @@ rustPlatform.buildRustPackage {
     hash = "sha256-jAN6M2yWEdpN4jG3EWDf4i2sMbj9qhYASKIsU2NVXqs=";
   };
 
+  cargoPatches = [ ./add-cargo-lock.patch ];
+
   cargoLock = {
     lockFile = ./Cargo.lock;
+    outputHashes = {
+      "smithay-client-toolkit-0.20.0" = "sha256-rFNSzm1003spYHdzlvCyb7PWGMOJibUq/Ek3F5HABkM=";
+    };
   };
 
   nativeBuildInputs = [ pkg-config ];
